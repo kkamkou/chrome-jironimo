04cdf1f479223bf38266f70ad7ee78694bebae8e
Kanstantsin Kamkou <kkamkou@bloopark.de>
2013-01-14 14:21:34 +0100

    workspaces settings

--- src/app/controllers/settings.js
+++ src/app/controllers/settings.js
@@ -1,13 +1,54 @@
 // default settings for the colors section
 if (!localStorage.colors) {
-  localStorage.colors = '{"theme": "default"}';
+  localStorage.colors = angular.toJson({theme: 'default'});
+}
+
+// default settings for the workspaces section
+if (!localStorage.workspaces) {
+  localStorage.workspaces = angular.toJson([{
+    title: 'Default',
+    query: 'assignee = currentUser() ORDER BY updatedDate DESC',
+    default: true
+  }]);
 }
 
 function SettingsController($scope) {
   // defining dynamic data
-  angular.forEach(['account', 'colors', 'timer'], function (name) {
-    $scope[name] = angular.fromJson(localStorage[name]);
-  });
+  angular.forEach(
+    ['account', 'colors', 'timer', 'workspaces'], function (name) {
+      $scope[name] = angular.fromJson(localStorage[name]);
+    }
+  );
+
+  $scope.workspaceAdd = function () {
+    if ($scope.workspaces.length > 10) {
+      return false;
+    }
+    $scope.workspaces.push({title: null, query: null, default: false});
+  };
+
+  $scope.workspaceSetAsDefault = function (workspace) {
+    angular.forEach($scope.workspaces, function(entry){
+      if (entry.default) {
+        entry.default = false;
+      }
+      entry.default = (entry === workspace);
+    });
+  };
+
+  $scope.workspaceRemove = function (workspace) {
+    if ($scope.workspaces.length < 2) {
+      return false;
+    }
+
+    $scope.workspaces = _.filter($scope.workspaces, function (entry) {
+      return entry !== workspace;
+    });
+
+    if (workspace.default) {
+      $scope.workspaceSetAsDefault($scope.workspaces[0]);
+    }
+  };
 
   /**
    * Saves settings
