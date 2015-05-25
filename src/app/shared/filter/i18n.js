c66dc0427ba7c88d99757c859364c0a809eb05da
Kanstantsin Kamkou <kkamkou@gmail.com>
2015-05-25 19:56:26 +0200

    useless "use strict" notations

--- src/app/shared/filter/i18n.js
+++ src/app/shared/filter/i18n.js
@@ -6,8 +6,6 @@
  * @license http://opensource.org/licenses/BSL-1.0 Boost Software License 1.0
  */
 
-'use strict';
-
 angular
   .module('jironimo.shared')
   .filter('i18n', function () {
--- src/app/shared/module.js
+++ src/app/shared/module.js
@@ -6,6 +6,4 @@
  * @license http://opensource.org/licenses/BSL-1.0 Boost Software License 1.0
  */
 
-'use strict';
-
 angular.module('jironimo.shared', []);
