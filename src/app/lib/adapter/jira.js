94c6bbb307191d8e892daca52427e0cbfd1456b7
Kanstantsin Kamkou <kkamkou@gmail.com>
2016-08-25 23:13:05 +0200

    adapter preparations

---
+++ src/app/lib/adapter/jira.js
@@ -0,0 +1,118 @@
+/**
+ * chrome-jironimo
+ *
+ * @author Kanstantsin Kamkou <2ka.by>
+ * @link http://github.com/kkamkou/chrome-jironimo
+ * @license http://opensource.org/licenses/BSL-1.0
+ */
+
+/*final public*/class Jira {
+  constructor(request, url, timeout) {
+    this._request = request;
+    this._url = '' + url;
+    this._timeout = parseInt(timeout, 10) || 10000;
+    this._cache = {};
+  }
+
+  /**
+   * Currently logged user
+   * @param {Function} callback
+   */
+  myself(callback) {
+    if (this._cache.myself) {
+      return callback(null, this._cache.myself);
+    }
+    this._makeRequest('/api/latest/myself', {}, (err, data) => {
+      if (!err) { this._cache.myself = data; }
+      callback(err, data);
+    });
+  }
+
+  /**
+   * Searches for issues using JQL
+   * @param {Object} data
+   * @param {Function} callback
+   */
+  search(data, callback) {
+    this._makeRequest('/api/latest/search', data, callback);
+  }
+
+  /**
+   * Returns the favourite filters of the logged-in user
+   * @param {Function} callback
+   */
+  filterFavourite(callback) {
+    this._makeRequest('/api/latest/filter/favourite', {}, callback);
+  }
+
+  /**
+   * Assigns an issue to a user
+   * @param {Number} issueId
+   * @param {String} userName
+   * @param {Function} callback
+   */
+  issueAssignee(issueId, data, callback) {
+    this._makeRequest(`/api/latest/issue/${issueId}/assignee`, data, callback);
+  }
+
+  /**
+   * Adds a new worklog entry to an issue
+   * @param {Number} issueId
+   * @param {Object} data
+   * @param {Function} callback
+   */
+  issueWorklog(issueId, data, callback) {
+    this._makeRequest(
+      `/api/latest/issue/${issueId}/worklog?adjustEstimate=auto`,
+      data, callback
+    );
+  }
+
+  /**
+   * Perform a transition on an issue
+   * @param {Number} issueId
+   * @param {Object} data
+   * @param {Function} callback
+   */
+  transitions(issueId, data, callback) {
+    this._makeRequest(
+      `/api/latest/issue/${issueId}/transitions?expand=transitions.fields`,
+      data, callback
+    );
+  }
+
+  /**
+   * Makes request with the data set
+   * @private
+   * @param {String} urn
+   * @param {Object} dataSet
+   * @param {Function} callback
+   */
+  _makeRequest(urn, dataSet, callback) {
+    var callOptions = {
+      method: 'GET',
+      url: `${this._url}/rest${urn}`,
+      cache: false,
+      data: dataSet,
+      responseType: 'json',
+      timeout: this._timeout,
+      headers: {ContentType: 'application/json; charset=UTF-8'}
+    };
+
+    // different method
+    if (callOptions.data._method) {
+      callOptions.method = callOptions.data._method.toUpperCase();
+      delete callOptions.data._method;
+    }
+
+    // angular params;data fix
+    if (callOptions.method === 'GET') {
+      callOptions.params = callOptions.data;
+      delete callOptions.data;
+    }
+
+    this._request.fetch(callOptions)
+      .then(r => callback(null, r.toJson()))
+      .catch(e => callback(e));
+  }
+}
--- src/app/lib/jira-api.js
+++ src/app/lib/jira-api.js
@@ -47,130 +47,17 @@ angular
     }]);
   }])
   .service('cjJira', ['$rootScope', 'cjSettings', '$http', '$filter', function ($rootScope, cjSettings, $http, $filter) {
-    const cache = {};
+    const adapter = new Jira();
 
-    /**
-     * Currently logged user
-     *
-     * @public
-     * @param {Function} callback
-     */
-    this.myself = function (callback) {
-      if (cache.myself) {
-        return callback(null, cache.myself);
-      }
+    console.log('adapter');
 
-      this._makeRequest('/api/latest/myself', {}, (err, data) => {
-        if (!err) {
-          cache.myself = data;
-        }
-        callback(err, data);
-      });
-    };
+    if (!config.url) {
+      return callback(new Error($filter('i18n')('jiraApiUrlRequired')));
+    }
 
-    /**
-     * Searches for issues using JQL
-     *
-     * @public
-     * @param {Object} data
-     * @param {Function} callback
-     */
-    this.search = function (data, callback) {
-      this._makeRequest('/api/latest/search', data, callback);
-    };
+    $http(callOptions)
+      .success(json => callback(null, json))
+      .error(err => callback(new Error(err || $filter('i18n')('jiraApiConnectionProblem'))));
 
-    /**
-     * Returns the favourite filters of the logged-in user
-     *
-     * @public
-     * @param {Function} callback
-     */
-    this.filterFavourite = function (callback) {
-      this._makeRequest('/api/latest/filter/favourite', {}, callback);
-    };
-
-    /**
-     * Assigns an issue to a user
-     *
-     * @public
-     * @param {Number} issueId
-     * @param {String} userName
-     * @param {Function} callback
-     */
-    this.issueAssignee = function (issueId, data, callback) {
-      this._makeRequest(`/api/latest/issue/${issueId}/assignee`, data, callback);
-    };
-
-    /**
-     * Adds a new worklog entry to an issue
-     *
-     * @public
-     * @param {Number} issueId
-     * @param {Object} data
-     * @param {Function} callback
-     */
-    this.issueWorklog = function (issueId, data, callback) {
-      this._makeRequest(
-        `/api/latest/issue/${issueId}/worklog?adjustEstimate=auto`,
-        data, callback
-      );
-    };
-
-    /**
-     * Perform a transition on an issue
-     *
-     * @public
-     * @param {Number} issueId
-     * @param {Object} data
-     * @param {Function} callback
-     */
-    this.transitions = function (issueId, data, callback) {
-      this._makeRequest(
-        `/api/latest/issue/${issueId}/transitions?expand=transitions.fields`,
-        data, callback
-      );
-    };
-
-    /**
-     * Makes request with the data set
-     *
-     * @private
-     * @param {String} urn
-     * @param {Object} dataSet
-     * @param {Function} callback
-     */
-    this._makeRequest = function (urn, dataSet, callback) {
-      var config = cjSettings.account;
-
-      if (!config.url) {
-        return callback(new Error($filter('i18n')('jiraApiUrlRequired')));
-      }
-
-      var callOptions = {
-        method: 'GET',
-        url: config.url + '/rest' + urn,
-        cache: false,
-        data: dataSet,
-        responseType: 'json',
-        timeout: config.timeout * 1000,
-        headers: {ContentType: 'application/json; charset=UTF-8'}
-      };
-
-      // different method
-      if (callOptions.data._method) {
-        callOptions.method = callOptions.data._method.toUpperCase();
-        delete callOptions.data._method;
-      }
-
-      // angular params;data fix
-      if (callOptions.method === 'GET') {
-        callOptions.params = callOptions.data;
-        delete callOptions.data;
-      }
-
-      // ajax object
-      $http(callOptions)
-        .success(json => callback(null, json))
-        .error(err => callback(new Error(err || $filter('i18n')('jiraApiConnectionProblem'))));
-    };
+    return adapter;
   }]);
