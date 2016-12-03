8d8e6225d755bccb858587d0efdb321c77e030ed
Kanstantsin Kamkou <kkamkou@gmail.com>
2016-12-03 05:28:37 +0100

    vendors up

--- update-vendors.sh
+++ update-vendors.sh
@@ -3,13 +3,13 @@
 set -e
 
 LIST=`cat << EOL
-https://code.angularjs.org/1.5.8/angular.min.js;vendors/angularjs/angular.js
-https://code.angularjs.org/1.5.8/angular-route.min.js;vendors/angularjs/angular-route.js
-https://code.angularjs.org/1.5.8/angular-sanitize.min.js;vendors/angularjs/angular-sanitize.js
+https://code.angularjs.org/1.5.9/angular.min.js;vendors/angularjs/angular.js
+https://code.angularjs.org/1.5.9/angular-route.min.js;vendors/angularjs/angular-route.js
+https://code.angularjs.org/1.5.9/angular-sanitize.min.js;vendors/angularjs/angular-sanitize.js
 https://code.jquery.com/jquery-3.1.1.min.js;vendors/jquery/jquery-latest.js
 https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js;vendors/less/less-latest.js
-https://cdn.jsdelivr.net/lodash/4.16.4/lodash.min.js;vendors/lodash/lodash-latest.js
-https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js;vendors/momentjs/moment-latest.js
+https://cdn.jsdelivr.net/lodash/4.17.2/lodash.min.js;vendors/lodash/lodash-latest.js
+https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.0/moment.min.js;vendors/momentjs/moment-latest.js
 EOL`
 
 for l in $LIST; do
