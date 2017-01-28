5906f29bd2e91d20923086263e083aa867fe7da1
Kanstantsin Kamkou <kkamkou@gmail.com>
2017-01-28 14:39:22 +0100

    deps up

--- update-vendors.sh
+++ update-vendors.sh
@@ -7,9 +7,9 @@ https://code.angularjs.org/1.5.9/angular.min.js;vendors/angularjs/angular.js
 https://code.angularjs.org/1.5.9/angular-route.min.js;vendors/angularjs/angular-route.js
 https://code.angularjs.org/1.5.9/angular-sanitize.min.js;vendors/angularjs/angular-sanitize.js
 https://code.jquery.com/jquery-3.1.1.min.js;vendors/jquery/jquery-latest.js
-https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.1/less.min.js;vendors/less/less-latest.js
-https://cdn.jsdelivr.net/lodash/4.17.2/lodash.min.js;vendors/lodash/lodash-latest.js
-https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.0/moment.min.js;vendors/momentjs/moment-latest.js
+https://cdnjs.cloudflare.com/ajax/libs/less.js/2.7.2/less.min.js;vendors/less/less-latest.js
+https://cdn.jsdelivr.net/lodash/4.17.4/lodash.min.js;vendors/lodash/lodash-latest.js
+https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js;vendors/momentjs/moment-latest.js
 EOL`
 
 for l in $LIST; do
